c9378a8fdc24b7d34a0cf290a549670e4eec42c3
Vmccmv <98213272+Vmccmv@users.noreply.github.com>
2022-01-22 21:28:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    titleWeb: "Belinhcutephomaique",
+    titleWeb: "I love you",
     introTitle: 'Bé Linh',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Cô bé xinh xắn yêu cute phô mai con bò cười :)) `,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Em có thích anh khôngggggg 🥰',
+    desc: 'Em có thích anh khôngggggg ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh  <3',
     reply: 'Mình thích thì mình yêu thôi <33333333',
-    mess: 'Anh biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Anh cũng yêu em nhiều nhiều bé yêu ạaa 😘😘',
     messDesc: 'Làm người yêu anh nhaaa!!!.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
